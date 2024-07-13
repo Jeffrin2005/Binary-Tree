d01d4289e1680071876a014e61e8ecd11fa8dec3
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-13 10:04:24 +0530

    (ITERATIVE)Search in a Binary Search Tree.cpp

--- (ITERATIVE)Search in a Binary Search Tree.cpp
+++ (ITERATIVE)Search in a Binary Search Tree.cpp
@@ -1,14 +1,5 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// time complexity = o(log(n));
+// space complexity = o(1)
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
