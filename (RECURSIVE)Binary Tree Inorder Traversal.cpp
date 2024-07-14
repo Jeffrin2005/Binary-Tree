6329be626492a40488a82b906f2baafc60df3836
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-14 18:07:03 +0530

    (RECURSIVE)Binary Tree Inorder Traversal.cpp

---
+++ (RECURSIVE)Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,18 @@
+// time complexity = o(n)
+// space complexity = o(log(n))
+class Solution {
+public:
+    void inorder(TreeNode* node, vector<int>&ans){
+        if(node == NULL) return;
+        inorder(node->left, ans);
+        ans.push_back(node->val);
+        inorder(node->right,ans);
+    
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        inorder(root, ans);
+        return ans; 
+        
+    }
+};
