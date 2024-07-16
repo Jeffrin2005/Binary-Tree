1403161697b2c6202c71642d3fe8a37730aaddff
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-16 22:21:24 +0530

    (RECURSIVE)Binary Tree Paths.cpp

---
+++ (RECURSIVE)Binary Tree Paths.cpp
@@ -0,0 +1,31 @@
+
+/**
+time complexity = o(n)
+space comnplexity = o(n);
+ */
+class Solution {
+    private:
+    void findPaths(TreeNode* node, string path, vector<string>& paths) {
+        if (node==NULL) return;
+        if(!path.empty()) {
+            path += "->";
+        }
+        path+=to_string(node->val);
+        // If it's a leaf node, add the path to the list
+       if (node->left == NULL && node->right == NULL){
+            paths.push_back(path);
+        }else{
+            // Otherwise, continue to explore the left and right subtree
+            findPaths(node->left, path, paths);
+            findPaths(node->right, path, paths);
+        }
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> paths;
+        if(root != NULL){
+            findPaths(root, "", paths);
+        }
+        return paths;
+    }
+};
