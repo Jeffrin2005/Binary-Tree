14140d9b52874ae79904d9e2ac2ec6805141db50
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-30 08:26:39 +0530

    (RECURSIVE)Minimum Distance Between BST Nodes.cpp

---
+++ (RECURSIVE)Minimum Distance Between BST Nodes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+// time comp = o(n) space comp = o(h)
+public:
+    TreeNode* prev = nullptr;
+    int minDiff = INT_MAX;
+    void inorder(TreeNode* node) {
+        if (node==NULL) return;
+        // Traverse the left subtree
+        inorder(node->left);
+        // Process the current node
+        if(prev != NULL){
+            minDiff = min(minDiff, node->val - prev->val);
+        }
+        prev = node;
+        // Traverse the right subtree
+        inorder(node->right);
+    }
+    int minDiffInBST(TreeNode* root){
+        inorder(root);
+        return minDiff;
+    }
+};
