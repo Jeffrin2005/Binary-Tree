7bd7576c97c89f083e246cef41bec0aeda73526b
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-11-02 17:58:22 +0530

    Update (RECURISIVE)-N-ary Tree Preorder Traversal.cpp

--- (RECURISIVE)-N-ary Tree Preorder Traversal.cpp
+++ (RECURISIVE)-N-ary Tree Preorder Traversal.cpp
@@ -3,6 +3,7 @@ public:
      void traverse(Node* node, std::vector<int>& result) {
         if(node == NULL) return;
         result.push_back(node->val);  // Visit the root of the subtree
+          
         for(auto&child : node->children) {  // Recursively visit all the children
             traverse(child, result);
         }
